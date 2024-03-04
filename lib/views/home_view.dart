727b1387420af2acbceb4aed6cf87c762eb29469
heldersantiago <heldersantiago273@gmail.com>
2024-03-04 13:52:07 +0100

    feat: home page done

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 
 class HomeView extends StatelessWidget {
-  List symptoms = ["Heart Rate", "Blood Pressure", "Cough", "Cold", "Fever"];
+  List symptoms = [
+    "Heart Rate",
+    "Blood Pressure",
+    "Cough",
+    "Cold",
+    "Fever",
+    "Headache",
+  ];
   List imgs = [
     "doctor1.jpg",
     "doctor2.jpg",
@@ -42,7 +49,7 @@ class HomeView extends StatelessWidget {
               InkWell(
                 onTap: () {},
                 child: Container(
-                  padding: EdgeInsets.all(20),
+                  padding: EdgeInsets.all(10),
                   decoration: BoxDecoration(
                       color: Color(0XFF7165D6),
                       borderRadius: BorderRadius.circular(10),
@@ -52,10 +59,180 @@ class HomeView extends StatelessWidget {
                             blurRadius: 6,
                             spreadRadius: 4)
                       ]),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        padding: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                            color: Colors.white, shape: BoxShape.circle),
+                        child:
+                            Icon(Icons.add, color: Color(0XFF7165D6), size: 35),
+                      ),
+                      SizedBox(height: 30),
+                      Text(
+                        "Adult Health",
+                        style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w500),
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        "Take Care of Your Health",
+                        style: TextStyle(color: Colors.white70),
+                      )
+                    ],
+                  ),
                 ),
-              )
+              ),
+              InkWell(
+                onTap: () {},
+                child: Container(
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                            color: Colors.black12,
+                            blurRadius: 6,
+                            spreadRadius: 4)
+                      ]),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        padding: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                            color: Color(0XFF7165D6), shape: BoxShape.circle),
+                        child: Icon(Icons.home_filled,
+                            color: Colors.white, size: 35),
+                      ),
+                      SizedBox(height: 30),
+                      Text(
+                        "Home Visit",
+                        style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.black,
+                            fontWeight: FontWeight.w600),
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        "Make an Appointment",
+                        style: TextStyle(color: Colors.black87),
+                      )
+                    ],
+                  ),
+                ),
+              ),
             ],
-          )
+          ),
+          SizedBox(height: 20),
+          Padding(
+              padding: EdgeInsets.only(left: 15),
+              child: Text(
+                "What are you Symptoms?",
+                style: TextStyle(
+                    color: Colors.black87,
+                    fontWeight: FontWeight.w600,
+                    fontSize: 20),
+              )),
+          SizedBox(
+              height: 70,
+              child: ListView.builder(
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  itemCount: symptoms.length,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      margin:
+                          EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                      padding: EdgeInsets.symmetric(horizontal: 25),
+                      decoration: BoxDecoration(
+                          color: Color(0XFFF4F6FA),
+                          borderRadius: BorderRadius.circular(10),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black12,
+                              blurRadius: 4,
+                              spreadRadius: 2,
+                            )
+                          ]),
+                      child: Center(
+                        child: Text(
+                          symptoms[index],
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.black87,
+                          ),
+                        ),
+                      ),
+                    );
+                  })),
+          SizedBox(height: 15),
+          Padding(
+              padding: EdgeInsets.only(left: 15),
+              child: Text(
+                "Popular Doctors",
+                style: TextStyle(
+                    color: Colors.black87,
+                    fontWeight: FontWeight.w600,
+                    fontSize: 20),
+              )),
+          GridView.builder(
+            gridDelegate:
+                SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+            itemCount: 4,
+            shrinkWrap: true,
+            physics: NeverScrollableScrollPhysics(),
+            itemBuilder: (context, index) {
+              return InkWell(
+                onTap: () {},
+                child: Container(
+                  margin: EdgeInsets.all(10),
+                  padding: EdgeInsets.symmetric(vertical: 15),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(5),
+                      boxShadow: [
+                        BoxShadow(
+                            color: Colors.black12,
+                            blurRadius: 4,
+                            spreadRadius: 2)
+                      ]),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      CircleAvatar(
+                        radius: 35,
+                        backgroundImage: AssetImage("images/doctors.png"),
+                      ),
+                      Text(
+                        "Dr. Jorge Veloso",
+                        style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.black87),
+                      ),
+                      Row(
+                        mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.star,
+                            color: Colors.amber,
+                          ),
+                          Text("Desenvolvedor")
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              );
+            },
+          ),
         ],
       ),
     );
