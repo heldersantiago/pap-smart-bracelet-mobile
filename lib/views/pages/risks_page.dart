bafd338a072a81be4bde17c2d6089b834ee5107e
heldersantiago <heldersantiago273@gmail.com>
2024-04-09 16:37:14 +0100

    Update notification Screen, alerts and risks

---
+++ lib/views/pages/risks_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RisksPage extends StatefulWidget {
+  const RisksPage({super.key});
+
+  @override
+  State<RisksPage> createState() => _AlertsPageState();
+}
+
+class _AlertsPageState extends State<RisksPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
