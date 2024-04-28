f3b7c38d34c73ff0a2dab59a3cbe2ce4742c20f4
heldersantiago <heldersantiago273@gmail.com>
2024-04-28 13:10:10 +0100

    line_chart.dart

--- lib/views/screens/splash_screen.dart
+++ lib/views/screens/splash_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:pap/routes.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -7,20 +9,26 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+    Future.delayed(Duration(seconds: 5), () {
+      Navigator.of(context).pushReplacementNamed(RouteGenerator.welcomePage);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        width: double.infinity,
-        decoration: const BoxDecoration(
-            gradient: LinearGradient(
-                colors: [Colors.blue, Colors.pink],
-                begin: Alignment.topRight,
-                end: Alignment.bottomLeft)),
-        child: const Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [Text("splash screen")],
+        color: const Color(0xFF290441),
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Image.asset(
+          "images/logo.png",
         ),
       ),
     );
